2f40e18a18572f001964616fe1a7fab7e3786f07
EC2 Default User <ec2-user@ip-172-31-46-120.us-west-2.compute.internal>
2018-04-25 02:53:55 +0000

    Use same table by golang Editor and js Editor

--- mySpryMedia/controllers/yacback.js
+++ mySpryMedia/controllers/yacback.js
@@ -8,11 +8,7 @@ let {
 	Options
 } = require("datatables.net-editor-server");
 router.all('/snapi/yacback', async function(req, res) {
-console.log('controller/yacback.js :');
-console.log('The req is: :');
-console.log(req.params);
-console.log('The req is finished :');
-	let editor = new Editor(db, 'yacback').fields(
+	let editor = new Editor(db, 'serversideyacback').fields(
 		new Field('tag'),
 		new Field('rn'),
 		new Field('en'),
